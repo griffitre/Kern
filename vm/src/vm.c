e1f5c4f0e350680be0cc9e67f835d55f867349f9
griffitre <griffitre@gmail.com>
2026-06-23 18:58:41 -0400

    FEAT: vm.c implemented

--- vm/src/vm.c
+++ vm/src/vm.c
@@ -3,6 +3,7 @@
 #include "memory.h"
 #include "telemetry.h"
 #include "opcodes.h"
+#include <stdio.h>
 
 // Main run function
 int vm_run(uint8_t *bytecode, long size){
@@ -12,6 +13,14 @@ int vm_run(uint8_t *bytecode, long size){
 
     // Unsigned 8 bit int to store opcode
     uint8_t opcode;
+
+    // Values to push/store values to/from the stack
+    int value1;
+    int value2;
+
+    // Unsigned 8 bit int to passed parameters in the bytecode, and another to store a loaded value
+    uint8_t passedParameter;
+    uint8_t loadedValue;
 
     // Run indefinitely
     while (1){
@@ -25,21 +34,216 @@ int vm_run(uint8_t *bytecode, long size){
         opcode = bytecode[pc];
         pc++;
 
-        // Decode and execute
+        // Decode and execute.
+        // All of the if/return things are just for if an operation fails, such as a stack pop or push failing, or an invalid operation, like dividing by 0
         switch (opcode){
 
-            case 1:
+            // Push operation
+            case OP_PUSH:
+                // Get the passed value (that is currently at the pc)
+                passedParameter = bytecode[pc];
+                
+                // Increment pc
+                pc++;
 
-            case 2:
+                // Push the value
+                if (stack_push((int)passedParameter) < 0){
+                    return 1;
+                }
 
-            case 3:
+                // Break
+                break;
 
-            // etc etc etc...
+
+            // Pop operation
+            case OP_POP:
+                // Just pop the top of the stack
+                if (stack_pop(NULL) < 0){
+                    return 1;
+                }
+
+                // Break
+                break;
+
+
+            // Add operation
+            case OP_ADD:
+                // Get the two values from the top of the stack
+                if (stack_pop(&value1) < 0 || stack_pop(&value2) < 0){
+                    return 1;
+                }
+
+                // Push the sum of the values
+                if (stack_push(value1 + value2) < 0){
+                    return 1;
+                }
+
+                // Break
+                break;
+
+            
+            // Sub operation
+            case OP_SUB:
+                // Get the two values from the top of the stack
+                if (stack_pop(&value1) < 0 || stack_pop(&value2) < 0){
+                    return 1;
+                }
+                
+                // Push the difference between them (by doing value2 - value1)
+                if (stack_push(value2 - value1) < 0){
+                    return 1;
+                }
+
+                // Break
+                break;
+
+            
+            // Mul operation
+            case OP_MUL:
+                // Get the two values from the top of the stack
+                if (stack_pop(&value1) < 0 || stack_pop(&value2) < 0){
+                    return 1;
+                }
+
+                // Push their product
+                if (stack_push(value1 * value2) < 0){
+                    return 1;
+                }
+
+                // Break
+                break;
+
+
+            // Div operation
+            case OP_DIV:
+                // Get the two values from the top of the stack
+                if (stack_pop(&value1) < 0 || stack_pop(&value2) < 0){
+                    return 1;
+                }
+
+                // Check if the first value is zero and return 1 if it is
+                if (value1 == 0){
+                    return 1;
+                }
+
+                // Push their quotient
+                if (stack_push(value2 / value1) < 0){
+                    return 1;
+                }
+
+                // Break
+                break;
+
+
+            // Store operation
+            case OP_STORE:
+                // Get the value from the top of the stack
+                if (stack_pop(&value1) < 0){
+                    return 1;
+                }
+
+                // Get the passed address (that is currently at the pc)
+                passedParameter = bytecode[pc];
+
+                // Increment pc
+                pc++;
+
+                // Store the value to the address
+                if (memory_store(passedParameter, (uint8_t)value1) < 0){
+                    return 1;
+                }
+
+                // Break
+                break;
+
+
+            // Load operation
+            case OP_LOAD:
+                // Get the address passed (that is currently at the pc)
+                passedParameter = bytecode[pc];
+
+                // Increment pc
+                pc++;
+
+                // Load the value from the address
+                if (memory_load(passedParameter, &loadedValue) < 0){
+                    return 1;
+                }
+
+                // Push the value onto the stack
+                if (stack_push((int)loadedValue) < 0){
+                    return 1;
+                }
+
+                // Break
+                break;
+
+            
+            // Jmp operation
+            case OP_JMP:
+                // Get the address passed (that is currently at the pc)
+                passedParameter = bytecode[pc];
+
+                // Increment the pc
+                pc++;
+
+                // Set the pc to the value passed
+                pc = (int)passedParameter;
+
+                // Break
+                break;
+
+
+            // Jmp if zero operation
+            case OP_JMP_IF_ZERO:
+                // Pop the top of the stack
+                if (stack_pop(&value1) < 0){
+                    return 1;
+                }
+
+                // Get the address passed (that is currently at the pc)
+                passedParameter = bytecode[pc];
+
+                // Increment the pc
+                pc++;
+
+                // Set the pc to the passed address if value1 = 0
+                if (value1 == 0){
+                    pc = (int)passedParameter;
+                }
+
+                // Break
+                break;
+
+            
+            // Print operation
+            case OP_PRINT:
+                // Pop the top of the stack
+                if (stack_pop(&value1) < 0){
+                    return 1;
+                }
+
+                // Print it to stdout
+                fprintf(stdout, "%d ", value1);
+
+                // Break
+                break;
+
+            
+            // Halt operation
+            case OP_HALT:
+                // Simply return 0
+                return 0;
+
+            
+            // Default for invalid opcode. Return 1
+            default:
+                return 1;
 
         }
 
         // Update telemetry
-        // Tick
+        telemetry_tick();
 
     }
 
