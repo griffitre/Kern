30e8475d500b115797405eabde3e3d268d2e3cfc
griffitre <griffitre@gmail.com>
2026-06-23 17:48:41 -0400

    FEAT: telemetry.c implemented

---
+++ vm/src/telemetry.c
@@ -0,0 +1,54 @@
+#include "telemetry.h"
+#include "memory.h"
+#include "stack.h"
+#include <time.h>
+#include <stdio.h>
+
+// Int to store the number of cycles
+static int cycles;
+
+// Clock to store the start time
+static clock_t startTime;
+
+// Definition of telemetry_init
+void telemetry_init(){
+
+    // Set cycles to 0
+    cycles = 0;
+
+    startTime = clock();
+
+}
+
+// Definition of telemetry_tick
+void telemetry_tick(){
+
+    // Just increase the cycles by 1
+    cycles++;
+
+}
+
+// Definition of telemetry_print
+void telemetry_print(){
+
+    // Get the end time
+    clock_t endTime = clock();
+
+    // Calculate the difference
+    clock_t elapsedTime = endTime - startTime;
+
+    // Print the number of cycles
+    printf("Number of cycles: %d\n", cycles);
+
+    // Print the elapsed time
+    printf("Elapsed time: %.4f seconds\n", elapsedTime);
+
+    // Call stack_depth to get the stack depth and print it
+    int sDepth = stack_depth();
+    printf("Stack depth: %d\n", sDepth);
+
+    // Call memory_dump to dump the ram, then print a new line right after
+    memory_dump();
+    printf("\n");
+
+}
