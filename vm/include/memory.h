bea978757c0916e9372606a675534b5fdea58dcc
griffitre <griffitre@gmail.com>
2026-06-23 16:17:36 -0400

    memory.h implemented

---
+++ vm/include/memory.h
@@ -0,0 +1,21 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+// Include stdint
+#include <stdint.h>
+
+// Define a constant for the ram size
+#define RAM_SIZE 256
+
+// Function headers
+// memory_init
+void memory_init();
+
+// memory_store
+int memory_store(uint8_t addr, uint8_t value);
+
+// memory_load
+int memory_load(uint8_t addr);
+
+// End definition
+#endif
