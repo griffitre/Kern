80e6691583911c75dffac8edc566ee71bf8dd627
griffitre <griffitre@gmail.com>
2026-06-23 16:33:42 -0400

    FEAT: stack.h implemented

---
+++ vm/include/stack.h
@@ -0,0 +1,18 @@
+#ifndef STACK_H
+#define STACK_H
+
+// Function headers
+// stack_init
+void stack_init();
+
+// stack_push
+int stack_push(int value);
+
+// stack_pop
+int stack_pop(int *out);
+
+// stack_depth
+int stack_depth();
+
+// End definition
+#endif
