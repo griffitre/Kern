11cded82bed2fb262a0a8252b5c7e9c0a20966c6
griffitre <griffitre@gmail.com>
2026-06-25 23:11:46 -0400

    FEAT: subprocess running set up

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -48,6 +48,39 @@ def main(stdscr):
     box.edit(enterSubmit)
     filePath = box.gather().strip()
 
+    # Run the assembler
+    assemblerResult = subprocess.run(["python3", "assembler/src/assembler.py", filePath], capture_output=True, text=True)
+
+    # Check if the assembler failed or if it succeeded
+    # Failed, show the error screen
+    if assemblerResult.returncode != 0:
+        # Fail/error screen (TODO)
+        pass
+
+    # Succeeeded, run the vm
+    else:
+        
+        # Derive the .bin path from the .krn path
+        binPath = filePath.replace(".krn", ".bin")
+
+        # Run the VM with the binPath
+        vmResult = subprocess.run(["./vm/bin/kern", binPath], capture_output=True, text=True)
+
+        # Split stdout into the PRINT instruction output and the telemetry output
+        parts = vmResult.stdout.split("----TELEMETRY----\n")
+
+        # Get the print output and strip leading/trailing whitespace
+        printOutput = parts[0].strip()
+
+        # Get the telemetry output and strip leading/trailing whitespace, as long as it didnt crash (which it shouldn't, im just including this for safety reasons)
+        if len(parts) > 1:
+            telemetryOutput = parts[1].strip()
+        # If it did crash, inform the user by setting telemetryOutput to "VM CRASHED"
+        else:
+            telemetryOutput = "VM CRASHED BEFORE TELEMETRY COULD BE PRINTED"
+        pass
+
+
     # Wait for keypress before exiting the program
     stdscr.getch()
 
