b86343e32daf9590d511bef17f6af84e188b9b5e
griffitre <griffitre@gmail.com>
2026-06-25 22:17:37 -0400

    FEAT: row addresses are now printed by memory_dump

--- vm/src/memory.c
+++ vm/src/memory.c
@@ -50,10 +50,15 @@ void memory_dump(){
     // Just loop through the ram array and print every byte in hex format, adding a newline every 16 bytes
     for (int i = 0; i < RAM_SIZE; i++){
 
+        // Print the row address at the start of each row
+        if (i % 16 == 0){
+            printf("0x%02X: ", i);
+        }
+
         // Print a byte
         printf("%02X ", ram[i]);
 
-        // Check if a newline should be printed
+        // Check if a newline should be printed (which it should be every 16 bytes)
         if ((i + 1) % 16 == 0){
             printf("\n");
         }
